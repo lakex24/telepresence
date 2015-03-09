d4ff16c5cf7f6f5699bdcf09e44db2eb022507da
Jing Liu <lakex24@gmail.com>
2015-03-08 22:29:14 -0700

    fix bugs

--- index.js
+++ index.js
@@ -1,38 +1,49 @@
+// var app = require('express')();
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
+
 var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var server = app.listen(5000);
+var io = require('socket.io').listen(server);
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
-app.set('port', (process.env.PORT || 5000));
-// app.use(express.static(__dirname + '/public'));
 
+
+// app.set('port', (process.env.PORT || 5000));
+// app.listen(app.get('port'), function() {
+//   console.log("Node app is running at localhost:" + app.get('port'));
+// });
+
+
+// app.use(express.static(__dirname + '/public'));
 // app.get('/', function(request, response) {
 //   response.send('Hello World!');
 // });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
-});
 
 io.on('connection', function(socket){
 
-  // socket.on('chat message', function(msg){   // register an event handler on socket
-  //   // console.log('message: ' + msg);
-  //   io.emit('chat message', msg);   // server broadcast msg to everyone
-  // });
+  socket.on('chat message', function(msg){   // register an event handler on socket
+    // console.log('message: ' + msg);
+    io.emit('chat message', msg);   // server broadcast msg to everyone
+  });
 
   console.log('a user connected');
 
-  console.log(socket.id);
+  // console.log(socket.id);
 
 });
+
+
+
 
 // http.listen(8000, function(){
 //   console.log('listening on *:8000');
 // });
+
 
 // var express = require('express');
 // var app = express();
