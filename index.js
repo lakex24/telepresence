163187b59a0c8c99166f2e72695d85fa004beb74
Jing Liu <lakex24@gmail.com>
2015-03-08 21:07:48 -0700

    update port setting

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
-// app.get('/', function(req, res){
-//   res.send('<h1>Hello world</h1>');
+app.set('port', (process.env.PORT || 5000));
+// app.use(express.static(__dirname + '/public'));
+
+// app.get('/', function(request, response) {
+//   response.send('Hello World!');
 // });
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
 
 io.on('connection', function(socket){
 
@@ -23,6 +30,20 @@ io.on('connection', function(socket){
 
 });
 
-http.listen(8000, function(){
-  console.log('listening on *:8000');
-});
+// http.listen(8000, function(){
+//   console.log('listening on *:8000');
+// });
+
+// var express = require('express');
+// var app = express();
+
+// app.set('port', (process.env.PORT || 5000));
+// app.use(express.static(__dirname + '/public'));
+
+// app.get('/', function(request, response) {
+//   response.send('Hello World!');
+// });
+
+// app.listen(app.get('port'), function() {
+//   console.log("Node app is running at localhost:" + app.get('port'));
+// });
